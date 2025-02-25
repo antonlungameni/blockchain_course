edeec79fa4e48d53d28ceb9ac1339800c5c5da6d
antonlungameni <ndelitunga.l@gmail.com>
2025-02-25 21:51:15 +0200

    Initial

---
+++ SimpleStorage.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19; // Stating the solidity Version
+
+contract SimpleStorage {
+    uint256 myFavouriteNumber; // Declaring a variable of type 'uint' (unintialised) has 0 value
+    uint256[] listOfFavouriteNumbers;
+
+    struct Person {
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    // Person public myFriend = Person({favouriteNumber : 17,name: "Pat"});
+
+    Person[] public listOfPeople; // [] Dynamic Array
+
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+    function store(uint256 _favouriteNumber) public {
+        myFavouriteNumber = _favouriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return myFavouriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        listOfPeople.push(
+            Person({favouriteNumber: _favouriteNumber, name: _name})
+        );
+
+        nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
+}
